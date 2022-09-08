f7f5150e03fa665fbaa3448f336aa3fc0c513c33
panizsz <szpani633@gmail.com>
2022-09-08 11:45:34 +0430

    version dev 3

--- index.js
+++ index.js
@@ -3,9 +3,16 @@
 function paniz ( a ,b ){
 
     let sum = a + b ;
+    let omit = a * b;
+    let taghsim = a / b;
 
-    return sum
+    let max =  [sum , omit , taghsim];
+
+    return max;
+
+
 }
 
 console.log(paniz(1,2));
+
 
